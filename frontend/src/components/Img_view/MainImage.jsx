124e3d4cacdaa7770fe2353ba10c4f073b489124
FilipsCalijs <filips.calijs@gmail.com>
2025-11-08 17:38:09 +0200

    fix with < and > btns

--- frontend/src/components/Img_view/MainImage.jsx
+++ frontend/src/components/Img_view/MainImage.jsx
@@ -2,29 +2,40 @@ import React from "react";
 
 class MainImage extends React.Component {
   render() {
+    const { currentImg, handlePrevImage, handleNextImage, inStock } = this.props;
+
     return (
-      <div className="relative">
-        <div className="absolute top-1/2 translate-y-[-50%] w-full flex justify-between">
-          <button
-            onClick={() => this.props.handlePrevImage()}
-            className="w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA]"
-          >
-            <img src="/caretLeft.svg" alt="" />
-          </button>
-          <button
-            onClick={() => {
-              this.props.handleNextImage();
-            }}
-            className="w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA]"
-          >
-            <img src="/caretRight.svg" alt="" />
-          </button>
-        </div>
+      <div className="relative w-[575px] h-[478px] flex justify-center items-center">
+      
         <img
-          className="w-[575px] h-[478px] object-contain"
-          src={this.props.currentImg}
+          className={`w-full h-full object-contain ${
+            !inStock ? "opacity-40" : ""
+          }`}
+          src={currentImg}
           alt="main-img"
         />
+
+       
+        {!inStock && (
+          <div className="absolute top-1/2 left-1/2 z-30 transform -translate-x-1/2 -translate-y-1/2 text-[24px] font-semibold text-[#8D8F9A] uppercase">
+            OUT OF STOCK
+          </div>
+        )}
+
+     
+        <button
+          onClick={handlePrevImage}
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA] z-20"
+        >
+          <img src="/caretLeft.svg" alt="Previous" />
+        </button>
+
+        <button
+          onClick={handleNextImage}
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[31.7px] h-[31.7px] flex justify-center items-center bg-[#000000BA] z-20"
+        >
+          <img src="/caretRight.svg" alt="Next" />
+        </button>
       </div>
     );
   }
